419570b906b67ed3793191e692a1f5f8d25cfd29
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-06 01:01:11 +0300

    Delete src-backup-20260803 directory

--- src-backup-20260803/modules/fixed-asset/dto/update-fixed-asset.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateFixedAssetDto } from './create-fixed-asset.dto';
-
-export class UpdateFixedAssetDto extends PartialType(CreateFixedAssetDto) {}
