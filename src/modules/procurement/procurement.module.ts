861c2b57062ee838aebc35877cb969455b86fb5f
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-05 07:00:29 +0300

    Update procurement.module.ts

--- src/modules/procurement/procurement.module.ts
+++ src/modules/procurement/procurement.module.ts
@@ -1,1 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProcurementController } from './procurement.controller';
+import { ProcurementService } from './procurement.service';
+import { Vendor } from './entities/vendor.entity';
+import { PurchaseRequisition } from './entities/purchase-requisition.entity';
+import { PurchaseOrder } from './entities/purchase-order.entity';
+import { PurchaseOrderItem } from './entities/purchase-order-item.entity';
+import { GoodsReceiptNote } from './entities/goods-receipt-note.entity';
+import { GrnItem } from './entities/grn-item.entity';
+import { VendorInvoice } from './entities/vendor-invoice.entity';
+import { ThreeWayMatch } from './entities/three-way-match.entity';
+import { StockItem } from './entities/stock-item.entity';
+import { StockMovement } from './entities/stock-movement.entity';
+import { ClientConsignment } from './entities/client-consignment.entity';
 
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Vendor, PurchaseRequisition, PurchaseOrder, PurchaseOrderItem,
+      GoodsReceiptNote, GrnItem, VendorInvoice, ThreeWayMatch,
+      StockItem, StockMovement, ClientConsignment,
+    ]),
+  ],
+  controllers: [ProcurementController],
+  providers: [ProcurementService],
+  exports: [ProcurementService],
+})
+export class ProcurementModule {}
