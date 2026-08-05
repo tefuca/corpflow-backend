32213520f8807023a197f2662eea59d1cca8ff1e
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-05 09:20:37 +0300

    Update finance.module.ts

--- src/modules/finance/finance.module.ts
+++ src/modules/finance/finance.module.ts
@@ -1,1 +1,39 @@
+// src/modules/finance/finance.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RbacModule } from '../rbac/rbac.module';
+import { FinanceController } from './finance.controller';
+import { FinanceService } from './finance.service';
+import { UnifiedPaymentService } from './services/unified-payment.service';
 
+// Finance entities
+import { Client } from './entities/client.entity';
+import { ChartOfAccount } from './entities/chart-of-account.entity';
+import { GlJournalEntry } from './entities/gl-journal-entry.entity';
+import { GlJournalLine } from './entities/gl-journal-line.entity';
+import { ClientInvoice } from './entities/client-invoice.entity';
+import { ClientInvoiceLine } from './entities/client-invoice-line.entity';
+import { ClientPayment } from './entities/client-payment.entity';
+import { AccountsPayable } from './entities/accounts-payable.entity';
+import { PaymentVoucher } from './entities/payment-voucher.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Client,
+      ChartOfAccount,
+      GlJournalEntry,
+      GlJournalLine,
+      ClientInvoice,
+      ClientInvoiceLine,
+      ClientPayment,
+      AccountsPayable,
+      PaymentVoucher,
+    ]),
+    RbacModule,
+  ],
+  controllers: [FinanceController],
+  providers: [FinanceService, UnifiedPaymentService],
+  exports: [FinanceService, UnifiedPaymentService],
+})
+export class FinanceModule {}
