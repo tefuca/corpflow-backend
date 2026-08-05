e91f4712ecc263fc785987eeb6d685339d3156d9
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-05 11:36:30 +0300

    Update common.module.ts

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,1 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Document } from './entities/document.entity';
+import { Notification } from './entities/notification.entity';
+import { AuditLog } from './entities/audit-log.entity';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Document, Notification, AuditLog])],
+  providers: [],
+  exports: [TypeOrmModule],
+})
+export class CommonModule {}
