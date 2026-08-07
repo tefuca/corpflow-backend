895ccff5ed504e87969c7e2141c9dff58591188f
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-07 14:53:15 +0300

    Update agent.entity.ts

--- src/modules/agent/entities/agent.entity.ts
+++ src/modules/agent/entities/agent.entity.ts
@@ -1,1 +1,22 @@
+// src/modules/agent/entities/agent.entity.ts
+@Column({ unique: true })
+agentCode: string;
 
+@Column({ nullable: true })
+transactionVolumeTotal: number;
+
+@Column({ nullable: true })
+customerCount: number;
+
+@Column({ nullable: true })
+clientId: number;
+
+@Column({ nullable: true })
+projectId: number;
+
+@Column({ nullable: true })
+dapId: number;
+
+@ManyToOne(() => Dap, (dap) => dap.agents, { nullable: true })
+@JoinColumn({ name: 'dapId' })
+dap: Dap;
