61b22057996e772f54dda890bbaac82705c41aae
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-07 17:00:23 +0300

    Update vendor-invoice.entity.ts

--- src/modules/procurement/entities/vendor-invoice.entity.ts
+++ src/modules/procurement/entities/vendor-invoice.entity.ts
@@ -1,1 +1,22 @@
+// src/modules/procurement/entities/vendor-invoice.entity.ts
+@Column({ nullable: true })
+poId: number;
 
+@ManyToOne(() => PurchaseOrder, (po) => po.invoices)
+@JoinColumn({ name: 'poId' })
+purchaseOrder: PurchaseOrder;
+
+@Column({ type: 'decimal', precision: 10, scale: 2 })
+total: number;
+
+@Column({ default: false })
+matchedPo: boolean;
+
+@Column({ default: false })
+matchedGrn: boolean;
+
+@Column({ default: false })
+threeWayMatched: boolean;
+
+@Column({ nullable: true })
+matchedAt: Date;
