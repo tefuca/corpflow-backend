476912bcd9c98b955e71d93f0d6019c7c13750a7
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-08 13:02:12 +0300

    Update current-user.decorator.ts

--- src/modules/auth/decorators/current-user.decorator.ts
+++ src/modules/auth/decorators/current-user.decorator.ts
@@ -1,9 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const CurrentUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    const user = request.user;
-    return data ? user?.[data] : user;
-  },
-);
+export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.user;
+});
