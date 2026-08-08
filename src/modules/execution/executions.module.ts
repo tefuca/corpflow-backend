0d06b6aeab8f2470c864ba7774d7aac8ef3e7781
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-08 12:44:37 +0300

    Update executions.module.ts

--- src/modules/execution/executions.module.ts
+++ src/modules/execution/executions.module.ts
@@ -1,1 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Execution } from './entities/execution.entity';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Execution])],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class ExecutionsModule {}
