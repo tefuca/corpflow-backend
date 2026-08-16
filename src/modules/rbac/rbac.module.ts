ca27ffe58b5d6e1a2cefe9e10fdaefe557d22991
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-16 23:32:48 +0300

    Update rbac.module.ts

--- src/modules/rbac/rbac.module.ts
+++ src/modules/rbac/rbac.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SeedService } from './services/seed.service';
 import { RbacService } from './services/rbac.service';
-import { RbacController } from './controllers/rbac.controller';
 import { Role } from './entities/role.entity';
-import { User } from './entities/user.entity';
-import { UserRole } from './entities/user-role.entity';
 import { SystemFunction } from './entities/system-function.entity';
 import { RolePermission } from './entities/role-permission.entity';
-import { PermissionGuard } from '../auth/guards/permission.guard';
-import { SeedService } from './services/seed.service';
+import { UserRole } from './entities/user-role.entity';
 
 @Module({
-  imports: [
-   imports: [TypeOrmModule.forFeature([Role, SystemFunction, RolePermission, UserRole])],
+  imports: [TypeOrmModule.forFeature([Role, SystemFunction, RolePermission, UserRole])],
   providers: [RbacService, SeedService],
   exports: [RbacService, SeedService],
 })
