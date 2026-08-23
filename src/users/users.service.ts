0be5615619ee2674331a2c6e90bf38b900ffdef7
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-23 16:06:52 +0300

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findByUsername(username: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { username } });
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
