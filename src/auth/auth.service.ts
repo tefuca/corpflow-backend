4297e611dc5957e1840a65ac5827fb005573992e
tefuca <59285839+tefuca@users.noreply.github.com>
2026-08-23 16:22:39 +0300

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,7 +20,11 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.id, role: user.role };
+    const payload = {
+      username: user.username,
+      sub: user.id,
+      role: user.role,
+    };
     return {
       access_token: this.jwtService.sign(payload),
       user,
